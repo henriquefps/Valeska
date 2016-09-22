f3f896f3fd015f2066831e17207a1e537f54678c
henriquefps <henriquefps@hotmail.com>
2016-09-22 13:25:04 -0300

    principal com imports

--- Whipi/src/GUI/Principal.java
+++ Whipi/src/GUI/Principal.java
@@ -1,10 +1,11 @@
 package GUI;
 
+import negocio.beans.*;
+import repositorio.*;
+
 public class Principal {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Oi igor");
+		
 	}
-
 }
