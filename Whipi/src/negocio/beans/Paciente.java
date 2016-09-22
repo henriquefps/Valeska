104f72d525e3b39a6649a24b15f5779114933e83
henriquefps <henriquefps@hotmail.com>
2016-09-22 00:34:06 -0300

    Repositorio paciente e ajustes em paciente

--- Whipi/src/negocio/beans/Paciente.java
+++ Whipi/src/negocio/beans/Paciente.java
@@ -1,5 +1,55 @@
 package negocio.beans;
 
-public class Paciente {
+import java.time.LocalDate;
 
+public class Paciente {
+	private String nomeDoPaciente;
+	private LocalDate dataDeNascimento;
+	private String cpf;
+	private String telefone;
+	
+	public Paciente(String nome, String cpf, LocalDate dataDeNascimento, String telefone){
+		this.nomeDoPaciente = nome;
+		this.dataDeNascimento = dataDeNascimento;
+		this.cpf = cpf;
+		this.telefone = telefone;
+	}
+	
+	public String toString() {
+		return "Nome do Paciente: " + nomeDoPaciente + ", Data de Nascimento: " + dataDeNascimento + ", CPF: " + cpf
+				+ ", telefone=" + telefone + "\n";
+	}
+
+	public String getNomeDoPaciente() {
+		return nomeDoPaciente;
+	}
+
+	public void setNomeDoPaciente(String nomeDoPaciente) {
+		this.nomeDoPaciente = nomeDoPaciente;
+	}
+
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
 }
---
+++ Whipi/src/repositorio/RepositorioPaciente.java
@@ -0,0 +1,33 @@
+package repositorio;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import negocio.beans.*;
+
+public class RepositorioPaciente {
+	private ArrayList<Paciente> todosOsPacientes = new ArrayList<Paciente>();
+
+	public void cadastrarPaciente(String nome, String cpf, LocalDate dataDeNascimento, String telefone){
+		this.todosOsPacientes.add(new Paciente(nome, cpf, dataDeNascimento, telefone));
+	}
+	public void removerPaciente(Paciente paciente){
+		this.todosOsPacientes.remove(paciente);
+	}
+	public void atualizarPaciente(Paciente paciente, String nome, String cpf, LocalDate dataDeNascimento, String telefone){
+		paciente.setNomeDoPaciente(nome);
+		paciente.setCpf(cpf);
+		paciente.setDataDeNascimento(dataDeNascimento);
+		paciente.setTelefone(telefone);
+	}
+	public Paciente pesquisarPeloNome(String nomeDoPaciente){
+		Paciente aux = null;
+		for (int i = 0; i < todosOsPacientes.size(); i++) {
+			if(todosOsPacientes.get(i).getNomeDoPaciente() == nomeDoPaciente){
+				aux = todosOsPacientes.get(i);
+				break;
+			}
+		}
+		return aux;
+	}
+}
