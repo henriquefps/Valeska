5e9aa7601ab845c9128d3227cd1f4556ca8185d4
Pedro <Pedro@Pedro-PC>
2016-09-22 14:04:56 -0300

    Recepcionista

--- Whipi/src/negocio/beans/Recepcionista.java
+++ Whipi/src/negocio/beans/Recepcionista.java
@@ -1,5 +1,112 @@
 package negocio.beans;
 
 public class Recepcionista {
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String rg;
+	private char sexo;
+	private int idade;
+	private double salario;
+
+	public Recepcionista(String nome ,String cpf,String rg,String telefone,char sexo,int idade,double salario)
+	{
+		this.nome = nome;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.telefone = telefone;
+		this.sexo = sexo;
+		this.idade = idade;
+		this.salario = salario;
+	}
+	
+	public Recepcionista()
+	{
+		this.nome = null;
+		this.cpf = null;
+		this.rg = null;
+		this.telefone = null;
+		this.sexo = ' ';
+		this.idade = 0;
+		this.salario = 0;
+	}
+
+
+
+	public boolean equals(Recepcionista a)
+	{
+		boolean resultado =  false;
+		String nome = a.getNome();
+		String cpf = a.getCpf();
+		String rg = a.getRg();
+
+		if ( (nome != null) && (cpf != null) && (rg != null) && (this.nome == nome) && (this.cpf == cpf) || (this.rg == rg))
+		{
+			resultado = true;
+		}
+
+		return resultado;
+	}
+	
+	public String toString()
+	{
+		String resultado;
+		String quebraLinha = System.getProperty("line.separator"); 
+		
+		resultado = "Nome - " + this.nome + quebraLinha + "cpf - " + this.cpf + quebraLinha
+				+ "Rg - " + this.rg + quebraLinha + "Telefone - " + this.telefone + quebraLinha
+				+ "Sexo - " + this.sexo + quebraLinha + "Idade - " + this.idade + quebraLinha
+				+"Salario - " + this.salario + quebraLinha;
+				
+		
+		return resultado;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+
 
 }
--- Whipi/src/repositorio/RepositorioRecepcionistas.java
+++ Whipi/src/repositorio/RepositorioRecepcionistas.java
@@ -1,5 +1,67 @@
 package repositorio;
 
-public class RepositorioRecepcionistas {
+import java.util.ArrayList;
 
+import negocio.beans.Recepcionista;
+
+public class RepositorioRecepcionistas {
+	private ArrayList<Recepcionista> recepcionista;
+	int proximo;
+	public RepositorioRecepcionistas()
+	{
+		this.recepcionista = new ArrayList<Recepcionista>();
+		this.proximo = 0;
+	}
+
+
+	public void cadastrar(Recepcionista recepcionista)
+	{
+		boolean result = this.recepcionista.contains(recepcionista);
+
+		if ( result == true)
+		{
+			System.out.println("Recepcionista Já cadastrado(a)");
+		}else{
+			this.recepcionista.add(proximo, recepcionista);
+			proximo++;
+		}
+	}
+	public void remover(Recepcionista recepcionista)
+	{
+		boolean result = this.recepcionista.contains(recepcionista);
+
+		if ( result == true)
+		{
+			System.out.println("Recepcionista Já cadastrado(a)");
+		}else{
+			this.recepcionista.remove(recepcionista);
+		}
+	}
+
+	public void atualizar(Recepcionista recepcionista , String nome,String cpf,String telefone,String rg,char sexo,int idade,double salario)
+	{
+		recepcionista.setNome(nome);
+		recepcionista.setIdade(idade);
+		recepcionista.setCpf(cpf);
+		recepcionista.setRg(rg);
+		recepcionista.setSalario(salario);
+		recepcionista.setSexo(sexo);
+		recepcionista.setTelefone(telefone);
+	}
+
+	public Recepcionista pesquisar(String cpf)
+	{
+		Recepcionista resultado = null;
+
+		for ( int i = 0 ; i < this.recepcionista.size() ; i++)
+		{
+			if(this.recepcionista.get(i).getCpf() == cpf)
+			{
+				resultado = recepcionista.get(i);
+			}
+
+		}
+		return resultado;
+
+	}
 }
