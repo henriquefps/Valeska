8c6a354dfcad635f9f7d43f9fdc0d4f17a0dda51
LAPTOP-C7D0LLR1\Wilder <wcarvalho98@gmail.com>
2016-09-29 00:29:26 -0300

    Supress warning da classe principal.
    Apenas um supresswarning para que não ficasse aparecendo no projeto do
    eclipse como se tivesse ocorrendo algum problema nessa classe.

--- Whipi/src/gui/Principal.java
+++ Whipi/src/gui/Principal.java
@@ -3,6 +3,7 @@ package gui;
 import negocio.beans.*;
 import repositorio.*;
 
+@SuppressWarnings("unused")
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Oi igor");
