3354316ce1e3fa38ca8725b90fb1d376c1c49877
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2016-09-28 23:41:09 -0300

    Aplicando herança

---
+++ Whipi/src/negocio/beans/Pessoa.java
@@ -0,0 +1,113 @@
+package negocio.beans;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String telefone;
+	private String celular;
+	private char sexo;
+	private Endereco endereco;
+	private LocalDate dataDeNascimento;
+	
+	public Pessoa(String nome, String cpf, String rg, String telefone, String celular, char sexo, Endereco endereco, LocalDate dataDeNascimento) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.telefone = telefone;
+		this.celular = celular;
+		this.sexo = sexo;
+		this.endereco = endereco;
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Pessoa other = (Pessoa) obj;
+		if (cpf == null) {
+			if (other.cpf != null) {
+				return false;
+			}
+		} else if (!cpf.equals(other.cpf)) {
+			return false;
+		}
+		return true;
+	}
+	
+}
