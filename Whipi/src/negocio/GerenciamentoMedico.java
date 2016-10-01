0bc0aa9ed7be6fab130dd9b1a30922bead989bc5
henriquefps <henriquefps@hotmail.com>
2016-10-01 17:47:18 -0300

    Salvar login no repositorio

--- Whipi/src/negocio/GerenciamentoMedico.java
+++ Whipi/src/negocio/GerenciamentoMedico.java
@@ -8,9 +8,11 @@ import negocio.beans.*;
 
 public class GerenciamentoMedico {
 	private RepositorioPessoas pessoas;
+	private RepositorioLogin logins;
 
 	public GerenciamentoMedico() {
 		this.pessoas = RepositorioPessoas.getInstance();
+		this.logins = RepositorioLogin.getInstance();
 	}
 
 	public void cadastrarMedico(String nome, String cpf, String rg, String telefone, String celular, char sexo, Endereco endereco,
@@ -18,6 +20,7 @@ public class GerenciamentoMedico {
 		Medico novo = new Medico(nome, cpf, rg, telefone, celular, sexo, endereco, dataDeNascimento, numCRM, senha);
 		if(!pessoas.existe(novo)){
 			pessoas.cadastrar(novo);
+			logins.cadastrarLogin(novo.getLogin());
 		}
 	}
 	
@@ -32,7 +35,10 @@ public class GerenciamentoMedico {
 	public void alterarMedico(String cpf, String nome, String novocpf, String rg, String telefone, String celular, char sexo,
 			Endereco endereco, LocalDate dataDeNascimento, int numCRM, String senha){
 		Medico aux = pesquisarMedico(cpf);
-		pessoas.atualizar(nome, novocpf, rg, telefone, celular, sexo, endereco, dataDeNascimento, numCRM, senha, aux);	
+		pessoas.atualizar(nome, novocpf, rg, telefone, celular, sexo, endereco, dataDeNascimento, numCRM, senha, aux);
+		Login doMedico = logins.pesquisarLogin(aux.getCpf());
+		doMedico.setCpf(novocpf);
+		doMedico.setSenha(senha);
 	}
 	
 	public Medico pesquisarMedico(String cpf){
