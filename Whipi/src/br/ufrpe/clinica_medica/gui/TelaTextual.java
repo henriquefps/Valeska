da6eb2b65e4ab5f210a06d2a67c47c06761635a5
Ingo Pastl <ingo.pastl@gmail.com>
2016-10-04 23:25:06 -0300

    Comecei a tela textual e tal

--- Whipi/src/br/ufrpe/clinica_medica/gui/TelaTextual.java
+++ Whipi/src/br/ufrpe/clinica_medica/gui/TelaTextual.java
@@ -1,5 +1,21 @@
 package br.ufrpe.clinica_medica.gui;
 
-public class TelaTextual {
+import java.util.Scanner;
 
+public class TelaTextual {
+	
+	private static Scanner scan;
+
+	public static void main(String[] args){
+		String cpf;
+		String senha;
+		scan = new Scanner(System.in);
+		
+		
+		System.out.println("===== Clinica Boladona =====");
+		System.out.printf("CPF: ");
+		cpf = scan.nextLine();
+		System.out.printf("Senha: ");
+		senha = scan.nextLine();
+	}
 }
