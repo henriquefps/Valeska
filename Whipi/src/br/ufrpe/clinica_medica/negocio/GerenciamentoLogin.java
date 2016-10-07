0be2908ada44ef3516f680c1b230803df138c265
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2016-10-07 00:02:32 -0300

    Add files via upload

---
+++ Whipi/src/br/ufrpe/clinica_medica/negocio/GerenciamentoLogin.java
@@ -0,0 +1,34 @@
+package br.ufrpe.clinica_medica.negocio;
+
+import br.ufrpe.clinica_medica.negocio.beans.Medico;
+import br.ufrpe.clinica_medica.negocio.beans.Pessoa;
+import br.ufrpe.clinica_medica.negocio.beans.Recepcionista;
+import br.ufrpe.clinica_medica.repositorio.RepositorioPessoas;
+
+public class GerenciamentoLogin {
+
+	private RepositorioPessoas pessoas;
+
+	public GerenciamentoLogin() {
+		this.pessoas = RepositorioPessoas.getInstance();
+	}
+	
+	public Pessoa efetuarLogin(String cpf, String senha) {
+		Pessoa individuo = pessoas.pesquisar(cpf);
+		boolean senhaCorreta = false;
+		if (individuo != null) {
+			if (individuo instanceof Medico && ((Medico) individuo).getSenha().equals(senha)) {
+				senhaCorreta = true;
+			} else if (individuo instanceof Recepcionista && ((Recepcionista) individuo).getSenha().equals(senha)) {
+				senhaCorreta = true;
+			} else if (senha.equals("admin") && cpf.equals("admin")) {
+				senhaCorreta = true;
+			}
+		}
+		if (!senhaCorreta) {
+			individuo = null;
+		}
+		return individuo;
+	}
+	
+}
