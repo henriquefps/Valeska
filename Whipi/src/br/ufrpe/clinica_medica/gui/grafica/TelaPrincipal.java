aabbe8f82350c370168104cd9b321aff831807d3
LAPTOP-C7D0LLR1\Wilder <wcarvalho98@gmail.com>
2017-02-03 23:28:45 -0200

    Tela principal
    Implementei a tela principal, juntamente com uma classe de teste e a
    classe controller dela. Ajustem da forma que quiserem, e abram o fxml no
    scene builder para alterarem o design!

---
+++ Whipi/src/br/ufrpe/clinica_medica/gui/grafica/TelaPrincipal.java
@@ -0,0 +1,22 @@
+package br.ufrpe.clinica_medica.gui.grafica;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class TelaPrincipal implements Initializable {
+	
+	@FXML private TextField textField;
+	@FXML private PasswordField passField;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+	}
+
+
+}
---
+++ Whipi/src/br/ufrpe/clinica_medica/gui/grafica/TesteDeTela.java
@@ -0,0 +1,28 @@
+package br.ufrpe.clinica_medica.gui.grafica;
+
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TesteDeTela extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		URL fxml = getClass().getResource("TelaPrincipal.fxml");
+		Parent parent = FXMLLoader.load(fxml);
+		primaryStage.setTitle("Clínica Médica");
+		primaryStage.setScene(new Scene(parent));
+		primaryStage.show();
+		
+	}
+
+}
