bb67c6593854224504f70c4f64a1fbc9d6e4a99d
LAPTOP-C7D0LLR1\Wilder <wcarvalho98@gmail.com>
2017-02-05 18:10:59 -0200

    Troca de telas + dialog box's
    A tela admin agora está acessível, contudo não consigo fechar a tela
    principal.

---
+++ Whipi/src/br/ufrpe/clinica_medica/gui/grafica/TelaAdmin.java
@@ -0,0 +1,37 @@
+package br.ufrpe.clinica_medica.gui.grafica;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TelaAdmin implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+	
+	@FXML
+	public void telaAdmin() {
+		URL url = getClass().getResource("TelaAdmin.fxml");
+		Parent parent = null;
+		try {
+			parent = FXMLLoader.load(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Stage s = new Stage();
+		s.setTitle("Admin");
+		s.setScene(new Scene(parent));
+		s.show();
+	}
+
+}
--- Whipi/src/br/ufrpe/clinica_medica/gui/grafica/TelaPrincipalController.java
+++ Whipi/src/br/ufrpe/clinica_medica/gui/grafica/TelaPrincipalController.java
@@ -1,11 +1,18 @@
 package br.ufrpe.clinica_medica.gui.grafica;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import br.ufrpe.clinica_medica.negocio.FachadaClinicaMedica;
+import br.ufrpe.clinica_medica.negocio.beans.Medico;
+import br.ufrpe.clinica_medica.negocio.beans.Pessoa;
+import br.ufrpe.clinica_medica.negocio.beans.Recepcionista;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -15,6 +22,7 @@ public class TelaPrincipalController implements Initializable {
 	@FXML private PasswordField psfSenha;
 	 
 	private FachadaClinicaMedica fachada;
+	private TelaAdmin admin;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -28,12 +36,43 @@ public class TelaPrincipalController implements Initializable {
 		String senha = psfSenha.getText();
 		System.out.println("Usuário: " + usuario);
 		System.out.println("Senha: " + senha);
-		//TODO Código para passar pra camanda de negocio e fazer as checagens e posteriormente passar pra uma suposta tela.
+		Pessoa resultado = fachada.efetuarLogin(usuario, senha);
+		if (resultado instanceof Recepcionista) {
+			//TODO Tela de recepcionista
+		} else if (resultado instanceof Medico) {
+			//TODO Tela de médico
+		} else if (resultado instanceof Pessoa) {
+			//Inicia a tela admin, mas não fecha a tela principal
+			//Pergunta: onde a tela principal está sendo invocada??
+			//TODO Fechar a tela principal ao invocar outra tela
+			admin = new TelaAdmin();
+			admin.telaAdmin();
+		} else {
+			this.dadosInvalidos();
+		}
 	}
 	
 	public void sair(){
-		System.exit(0);
-	}
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Confirmação");
+		alert.setHeaderText("Sair");
+		alert.setContentText("Deseja fechar o programa?");
 
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK){
+			System.exit(0);
+		} else {
+		    alert.close();
+		}
+	}
+	
+	public void dadosInvalidos() {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Erro de dados!");
+		alert.setHeaderText("Dados inválidos!");
+		alert.setContentText("Usuário ou senha inválidos.");
+
+		alert.showAndWait();
+	}
 
 }
