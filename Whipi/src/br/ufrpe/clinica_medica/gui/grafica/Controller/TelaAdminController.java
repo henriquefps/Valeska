10fa02c68a3bc13071af7f8e8f4cc0f4a3cf1a05
LAPTOP-C7D0LLR1\Wilder <wcarvalho98@gmail.com>
2017-02-14 23:41:44 -0200

    Opção logoff e sair na tela admin.

--- Whipi/src/br/ufrpe/clinica_medica/gui/grafica/Controller/TelaAdminController.java
+++ Whipi/src/br/ufrpe/clinica_medica/gui/grafica/Controller/TelaAdminController.java
@@ -2,6 +2,7 @@ package br.ufrpe.clinica_medica.gui.grafica.Controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -9,7 +10,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class TelaAdminController implements Initializable {
@@ -85,5 +89,36 @@ public class TelaAdminController implements Initializable {
 	@FXML
 	private void telaRemovePaciente() {
 
+	}
+	
+	@FXML
+	private void logoff() {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Confirmação");
+		alert.setHeaderText("Logoff");
+		alert.setContentText("Deseja fazer logoff?");
+
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK){
+			t.voltarTela();
+			t.abrirTela();
+		} else {
+		    alert.close();
+		}
+	}
+	
+	@FXML
+	private void sair() {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Confirmação");
+		alert.setHeaderText("Sair");
+		alert.setContentText("Deseja fechar o programa?");
+
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK){
+			System.exit(0);
+		} else {
+		    alert.close();
+		}
 	}
 }
