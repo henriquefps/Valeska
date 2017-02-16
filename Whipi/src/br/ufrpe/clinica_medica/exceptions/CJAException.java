d29f2773267f1f824111e5202600c5e29358413c
henriquefps <henriquefps@hotmail.com>
2017-02-15 21:56:23 -0300

    Pedro encontrou minhas traps

--- Whipi/src/br/ufrpe/clinica_medica/exceptions/CJAException.java
+++ Whipi/src/br/ufrpe/clinica_medica/exceptions/CJAException.java
@@ -1,5 +1,19 @@
 package br.ufrpe.clinica_medica.exceptions;
 
-public class CJAException {
+import br.ufrpe.clinica_medica.negocio.beans.Consulta;
 
+/*
+ * Consulta ja agendada exception
+ */
+public class CJAException extends Exception {
+	private Consulta aSerTratada;
+	
+	public CJAException(Consulta a){
+		super("Esta Consulta esta cadastrada no dia a ser cancelado");
+		aSerTratada = a;
+	}
+	
+	public Consulta getASerTratada(){
+		return aSerTratada;
+	}
 }
--- Whipi/src/br/ufrpe/clinica_medica/negocio/GerenciamentoConsulta.java
+++ Whipi/src/br/ufrpe/clinica_medica/negocio/GerenciamentoConsulta.java
@@ -124,5 +124,13 @@ public class GerenciamentoConsulta {
 		} else {
 			throw new PNEException("CPF do medico nao encontrado no sistema");
 		}
+		
+		
+	}
+	
+	public void removerConsulta(Consulta c) throws CNEException {
+		if(c != null){
+			consultas.excluir(c);
+		}
 	}
 }
