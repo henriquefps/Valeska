65250f33e7bb5e3beacd25aaa9084aa6baac8972
henriquefps <henriquefps@hotmail.com>
2017-02-18 21:59:20 -0300

    Controller e FXML para Cadastrar os dias em que o médico vai trabalhar

---
+++ Whipi/src/br/ufrpe/clinica_medica/gui/grafica/Controller/DialogDiasDeTrabalhoController.java
@@ -0,0 +1,39 @@
+package br.ufrpe.clinica_medica.gui.grafica.Controller;
+
+import java.awt.Checkbox;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+
+public class DialogDiasDeTrabalhoController {
+	@FXML private Checkbox ckbDomingo;
+	@FXML private Checkbox ckbSegunda;
+	@FXML private Checkbox ckbTerca;
+	@FXML private Checkbox ckbQuarta;
+	@FXML private Checkbox ckbQuinta;
+	@FXML private Checkbox ckbSexta;
+	@FXML private Checkbox ckbSabado;
+	
+	@FXML private MenuButton menuDomingoInicio;
+	@FXML private MenuButton menuDomingoFim;
+	@FXML private MenuButton menuSegundaInicio;
+	@FXML private MenuButton menuSegundaFim;
+	@FXML private MenuButton menuTercaInicio;
+	@FXML private MenuButton menuTercaFim;
+	@FXML private MenuButton menuQuartaInicio;
+	@FXML private MenuButton menuQuartaFim;
+	@FXML private MenuButton menuQuintaInicio;
+	@FXML private MenuButton menuQuintaFim;
+	@FXML private MenuButton menuSextaInicio;
+	@FXML private MenuButton menuSextaFim;
+	@FXML private MenuButton menuSabadoInicio;
+	@FXML private MenuButton menuSabadoFim;
+	
+	@FXML public void salvarAlteracoes(){
+		
+	}
+	
+	@FXML public void cancelarAlteracoes(){
+		
+	}
+}
